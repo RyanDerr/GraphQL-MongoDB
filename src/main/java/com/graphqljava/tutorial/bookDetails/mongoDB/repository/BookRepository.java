b2f9f77e4ffa10de231504303e248a129083b544
Ryan Derr <54389874+RyanDerr@users.noreply.github.com>
2022-12-17 13:22:24 -0600

    Add Query Author By Last Name And Query Book By Title (#5)

--- src/main/java/com/graphqljava/tutorial/bookDetails/mongoDB/repository/BookRepository.java
+++ src/main/java/com/graphqljava/tutorial/bookDetails/mongoDB/repository/BookRepository.java
@@ -2,7 +2,11 @@ package com.graphqljava.tutorial.bookDetails.mongoDB.repository;
 
 import com.graphqljava.tutorial.bookDetails.mongoDB.entity.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface BookRepository extends MongoRepository<Book, String> {
-
+    List<Book> getBookByTitleIsIgnoreCase(String title);
 }
